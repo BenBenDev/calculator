abaa97b1e67fbb5c07447923c73a68cab61670b1
BenBenDev <benben.dev@gmail.com>
2018-01-25 15:15:38 +0100

    added to gradle : javadoc, cpd

--- src/main/java/com/imie/calculator/Addition.java
+++ src/main/java/com/imie/calculator/Addition.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.imie.calculator;
 
@@ -9,16 +9,26 @@ package com.imie.calculator;
  */
 public class Addition {
 
-	protected int value = 0;
-	
-	public Addition(int a) {
-		this.value = a;
-	}
-	public void make(int b) {
-		this.value += b;		
-	}
-	
-	public int getValue() {
-		return this.value;
-	}
+    protected int value = 0;
+/**
+ * sets the initial value
+ * @param a
+ */
+    public Addition(int a) {
+        this.value = a;
+    }
+    /**
+     * add b to initial value
+     * @param b
+     */
+    public void make(int b) {
+        this.value += b;
+    }
+    /**
+     * getter for Addition.value
+     * @return the value of value
+     */
+    public int getValue() {
+        return this.value;
+    }
 }
--- src/main/java/com/imie/calculator/Application.java
+++ src/main/java/com/imie/calculator/Application.java
@@ -1,25 +1,29 @@
-/**
- * 
- */
+/********************
+ * (c)2018 IMIE
+ * application in java to test Jenkins
+ ********************/
 package com.imie.calculator;
 
 /**
  * @author BenBen
- *
+ * application in java to test Jenkins
  */
 public class Application {
-	
-	public static void main(String[] args) {
-		int a=2; // useless ; just for test
+    
+/**
+ *main application
+ */
+    public static void main() {
+        int a=2; // useless ; just for test
 
-		Addition add  = new Addition(1);
-	  
-	  add.make (2);
-	  System.out.println(add.getValue());
-	  
-	  add.make (3);
-	  System.out.println(add.getValue());
-	  
-	  System.out.println("END");
-	}
+        Addition add  = new Addition(1);
+      
+      add.make (2);
+      System.out.println(add.getValue());
+      
+      add.make (3);
+      System.out.println(add.getValue());
+      
+      System.out.println("END");
+    }
 }
