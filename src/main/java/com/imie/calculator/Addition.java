9a04f73f7585241141a3c4e344d02d75108edeeb
BenBenDev <benben.dev@gmail.com>
2018-01-25 17:50:20 +0100

    fix to improve checkstyle score

--- src/main/java/com/imie/calculator/Addition.java
+++ src/main/java/com/imie/calculator/Addition.java
@@ -5,27 +5,26 @@ package com.imie.calculator;
 
 /**
  * @author BenBen
- *
  */
 public class Addition {
 
     protected int value = 0;
 /**
- * sets the initial value
+ * sets the initial value.
  * @param a
  */
     public Addition(int a) {
         this.value = a;
     }
     /**
-     * add b to initial value
+     * add b to initial value.
      * @param b
      */
     public void make(int b) {
         this.value += b;
     }
     /**
-     * getter for Addition.value
+     * getter for Addition.value.
      * @return the value of value
      */
     public int getValue() {
--- src/main/java/com/imie/calculator/Application.java
+++ src/main/java/com/imie/calculator/Application.java
@@ -1,29 +1,27 @@
 /********************
- * (c)2018 IMIE
+ * (c)2018 IMIE.
  * application in java to test Jenkins
  ********************/
 package com.imie.calculator;
 
 /**
- * @author BenBen
- * application in java to test Jenkins
+ * @author BenBen application in java to test Jenkins.
  */
 public class Application {
-    
-/**
- *main application
- */
-    public static void main() {
-        int a=2; // useless ; just for test
 
-        Addition add  = new Addition(1);
-      
-      add.make (2);
-      System.out.println(add.getValue());
-      
-      add.make (3);
-      System.out.println(add.getValue());
-      
-      System.out.println("END");
+    /**
+     * main application
+     */
+    public static void main() {
+
+        Addition add = new Addition(1);
+
+        add.make(2);
+        System.out.println(add.getValue());
+
+        add.make(3);
+        System.out.println(add.getValue());
+
+        System.out.println("END");
     }
 }
